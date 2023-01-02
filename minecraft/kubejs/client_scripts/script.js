af80e529232fcbed59a4a7e1db132591a4c67700
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-01-02 07:43:11 -0700

    Update script.js
    fixed JEI hiding recipes

--- minecraft/kubejs/client_scripts/script.js
+++ minecraft/kubejs/client_scripts/script.js
@@ -4,6 +4,12 @@
 
 const REMOVED_ITEMS = ["create_sa:copper_pickaxe", "create_sa:copper_axe", "create_sa:copper_shovel", "create_sa:copper_hoe", "create_sa:copper_helmet", "create_sa:copper_chestplate", "create_sa:copper_leggings", "create_sa:copper_boots", "create_sa:copper_sword", "easy_emerald:wooden_treeaxe","easy_emerald:wooden_excavator","easy_emerald:wooden_paxel","easy_emerald:wooden_hammer"];
 
-JEIEvents.hideItems('item', event => {
-	REMOVED_ITEMS.forEach(id => event.hide((id)));
+// loop over REMOVED_ITEMS array
+REMOVED_ITEMS.forEach((id) => {
+	// Remove recipes from JEI		
+	JEIEvents.hideItems(
+		('jei.hide.items', event => {
+			event.hide( id )
+		})	
+	);
 });
