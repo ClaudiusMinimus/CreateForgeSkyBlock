cfe52f04623e08e05ffd0f9b4b414fa1084d3553
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-02-03 15:50:06 -0700

    added quartz sifting mesh

--- minecraft/kubejs/startup_scripts/script.js
+++ minecraft/kubejs/startup_scripts/script.js
@@ -6,6 +6,7 @@ StartupEvents.registry('item', event => {
 	// Register new items here
 	// event.create('example_item').displayName('Example Item')
 	event.create('amethyst_mesh','createsifter:mesh').displayName('Amethyst Mesh');
+	event.create('quartz_mesh','createsifter:mesh').displayName('Quartz Mesh');
 })
 
 StartupEvents.registry('block', event => {
