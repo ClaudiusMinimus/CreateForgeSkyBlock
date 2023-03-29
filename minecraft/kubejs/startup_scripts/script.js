bc5e0e567c1dc6ee8d0cea9933f522dc328ed151
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-03-28 23:06:58 -0600

    still need to use temp fix to show mesh textures in JEI and inventory

--- minecraft/kubejs/startup_scripts/script.js
+++ minecraft/kubejs/startup_scripts/script.js
@@ -2,14 +2,13 @@
 
 // console.info('Hello, World! (You will only see this line once in console, during startup)')
 
-/* temp commit while waiting for mod update
-StartupEvents.registry('item', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
-	event.create('amethyst_mesh','createsifter:mesh').displayName('Amethyst Mesh');
-	event.create('quartz_mesh','createsifter:mesh').displayName('Quartz Mesh');
-})
- */
+// temp commit while waiting for mod update
+// StartupEvents.registry('item', event => {
+// 	// Register new items here
+// 	// event.create('example_item').displayName('Example Item')
+// 	event.create('amethyst_mesh','createsifter:mesh').displayName('Amethyst Mesh');
+// 	event.create('quartz_mesh','createsifter:mesh').displayName('Quartz Mesh');
+// })
 
 // Temp fix 
 StartupEvents.registry('item', event => {
@@ -19,7 +18,7 @@ StartupEvents.registry('item', event => {
 	event.create('quartz_mesh','createsifter:mesh').displayName('Quartz Mesh').parentModel("createsifter:block/meshes/custom_mesh").texture("1","kubejs:item/quartz_mesh").texture("0","kubejs:block/coal_block");
 })
 
-StartupEvents.registry('block', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
+// StartupEvents.registry('block', event => {
+// 	// Register new blocks here
+// 	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+// })
